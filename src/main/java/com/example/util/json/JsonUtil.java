e459df2780aa3cb2d1727f2c63986e2003305467
1CSH1 <602685158@qq.com>
2017-03-29 23:14:54 +0800

    add the fastjson jar and the simple method of json to object | content: the methods have not achieve

---
+++ src/main/java/com/example/util/json/JsonUtil.java
@@ -0,0 +1,50 @@
+package com.example.util.json;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author James-CSH
+ * @since 17-3-29 下午10:41
+ */
+public class JsonUtil {
+
+    /**
+     * object to json
+     */
+
+    public static String list2json() {
+
+    }
+
+    public static String map2json() {
+
+    }
+
+    public static String array2json() {
+
+    }
+
+    // ....
+
+
+    /**
+     * json to object
+     */
+
+    public static List json2list() {
+
+    }
+
+    public static Map json2map() {
+
+    }
+
+    public static Object[] json2array() {
+
+    }
+
+    // ....
+
+}
