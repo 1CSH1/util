1ead616e42b034c942d34ba7ff06d07e4de4248b
1CSH1 <602685158@qq.com>
2017-04-07 22:57:53 +0800

    title: realize array2xml, list2xml in XmlUtil | content: realize array2xml and list2xml method

--- src/main/java/com/example/util/xml/XmlUtil.java
+++ src/main/java/com/example/util/xml/XmlUtil.java
@@ -26,8 +26,11 @@ public class XmlUtil {
      * object transfarm to xml
      *
      * @param object
+     *              the object will transform to xml
      * @param <T>
+     *              the real object
      * @return
+     *              the xml string of object
      */
     public static <T> String object2xml(T object) {
         if (null == object) {
@@ -35,9 +38,9 @@ public class XmlUtil {
         }
 
         Document document = DocumentHelper.createDocument();
-        //get the object simple name
+        // get the object simple name
         String objectName = object.getClass().getSimpleName();
-        //get all fields of the object
+        // get all fields of the object
         Field[] objectProperties = object.getClass().getDeclaredFields();
         // set the root element
         Element root = document.addElement(objectName);
@@ -63,12 +66,82 @@ public class XmlUtil {
         return document.asXML();
     }
 
-    public static String array2xml() {
-        return null;
+    public static <T> String array2xml(T[] objects) {
+        if (null == objects || objects.length <= 0) {
+            return "";
+        }
+
+        T object = objects[0];
+        Document document = DocumentHelper.createDocument();
+        Element root = document.addElement(object.getClass().getSimpleName() + "s");
+        try {
+            String methodStr = "";
+            // traverse the objects
+            for (int i=0; i<objects.length; i++) {
+                object = objects[i];
+                Element objectRoot = root.addElement(object.getClass().getSimpleName());
+
+                // get all fields of the object
+                Field[] objectProperties = object.getClass().getDeclaredFields();
+                // set the root element
+                for (Field property : objectProperties) {
+                    methodStr = "get" + property.getName().substring(0, 1).toUpperCase() + property.getName().substring(1);
+                    Method method = object.getClass().getMethod(methodStr);
+                    // add the element
+                    Object obj = method.invoke(object);
+                    if (null != obj) {
+                        objectRoot.addElement(property.getName()).setText(obj.toString());
+                    }
+                }
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return document.asXML();
     }
 
-    public static String list2xml() {
-        return null;
+    public static <T> String list2xml(List<T> list) {
+        if (null == list || list.isEmpty()) {
+            return "";
+        }
+
+        T object = list.get(0);
+        Document document = DocumentHelper.createDocument();
+        Element root = document.addElement(object.getClass().getSimpleName() + "s");
+        try {
+            String methodStr = "";
+            // traverse the objects
+            for (int i=0; i<list.size(); i++) {
+                object = list.get(i);
+                Element objectRoot = root.addElement(object.getClass().getSimpleName());
+
+                // get all fields of the object
+                Field[] objectProperties = object.getClass().getDeclaredFields();
+                // set the root element
+                for (Field property : objectProperties) {
+                    methodStr = "get" + property.getName().substring(0, 1).toUpperCase() + property.getName().substring(1);
+                    Method method = object.getClass().getMethod(methodStr);
+                    // add the element
+                    Object obj = method.invoke(object);
+                    if (null != obj) {
+                        objectRoot.addElement(property.getName()).setText(obj.toString());
+                    }
+                }
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return document.asXML();
     }
 
     public static String map2xml() {
--- src/test/java/com/example/util/xml/TestXmlUtil.java
+++ src/test/java/com/example/util/xml/TestXmlUtil.java
@@ -29,9 +29,107 @@ public class TestXmlUtil {
         List<Person> children = new ArrayList<Person>();
         children.add(child);
 
-//        person.setChildren(children);
 System.out.println(XmlUtil.object2xml(person));
+/*
+<?xml version="1.0" encoding="utf-8"?>
+
+<Person>
+  <id>12</id>
+  <name>James勒布朗</name>
+  <age>32</age>
+  <gender>1</gender>
+</Person>
+
+*/
     }
 
+    @Test
+    public void testArray2Xml() {
+        Person[] persons = new Person[10];
+        for (int i=0; i<persons.length; i++) {
+            Person person = new Person();
+            person.setId(i);
+            person.setName("James勒布朗" + i);
+            person.setAge(30 + i);
+            person.setGender(i > 5 ? 1 : 0);
+            Person child = new Person();
+            child.setId(i * 10 + i);
+            child.setName("James勒布朗" + 10 + i);
+            child.setAge(i);
+            child.setGender(1);
+            child.setChildren(null);
+
+            List<Person> children = new ArrayList<Person>();
+            children.add(child);
+            persons[i] = person;
+        }
+        System.out.println(XmlUtil.array2xml(persons));
+/*
+<?xml version="1.0" encoding="utf-8"?>
+<Persons>
+  <Person>
+    <id>0</id>
+    <name>James勒布朗0</name>
+    <age>30</age>
+    <gender>0</gender>
+  </Person>
+  <Person>
+    <id>1</id>
+    <name>James勒布朗1</name>
+    <age>31</age>
+    <gender>0</gender>
+  </Person>
+  <Person>
+    <id>2</id>
+    <name>James勒布朗2</name>
+    <age>32</age>
+    <gender>0</gender>
+  </Person>
+  <Person>
+    <id>3</id>
+    <name>James勒布朗3</name>
+    <age>33</age>
+    <gender>0</gender>
+  </Person>
+  <Person>
+    <id>4</id>
+    <name>James勒布朗4</name>
+    <age>34</age>
+    <gender>0</gender>
+  </Person>
+  <Person>
+    <id>5</id>
+    <name>James勒布朗5</name>
+    <age>35</age>
+    <gender>0</gender>
+  </Person>
+  <Person>
+    <id>6</id>
+    <name>James勒布朗6</name>
+    <age>36</age>
+    <gender>1</gender>
+  </Person>
+  <Person>
+    <id>7</id>
+    <name>James勒布朗7</name>
+    <age>37</age>
+    <gender>1</gender>
+  </Person>
+  <Person>
+    <id>8</id>
+    <name>James勒布朗8</name>
+    <age>38</age>
+    <gender>1</gender>
+  </Person>
+  <Person>
+    <id>9</id>
+    <name>James勒布朗9</name>
+    <age>39</age>
+    <gender>1</gender>
+  </Person>
+</Persons>
+
+*/
+    }
 
 }
